ce33d7184d4a5e4344a8f541aa6156e4fdbdf8a2
Nam Nom Nguyen <CongNam18165@gmail.com>
2022-09-06 23:40:48 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,29 @@
+
 const express = require('express');
 const morgan= require('morgan');
 const app = express();
 const path = require('path');
+const handlebars = require('express-handlebars')
+
 
 const port = 3000;
+//HTTP Logger
+app.use(morgan('combined'));
 
-app.use(morgan('combined'))
-
+//Template engine
+app.engine('hbs',handlebars.engine({extname : '.hbs'}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', (req, res) => {
 
-  res.send('hello world!')
+  res.render('home');
 })
 
+app.get('/news', (req, res) => {
+
+  res.render('news');
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
